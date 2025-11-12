cd226459f3bed0d5d9c6697b1f3cc29f4910ed35
danielolafo <danielolafo@gmail.com>
2025-11-12 09:00:06 -0500

    Added temporal debug log for heatlh endpoint

--- src/main/java/com/drone/delivery/controller/HealthController.java
+++ src/main/java/com/drone/delivery/controller/HealthController.java
@@ -1,20 +1,22 @@
 package com.drone.delivery.controller;
 
-import java.time.Duration;
 import java.time.LocalDate;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/health")
+@Slf4j
 public class HealthController {
 	
 	@GetMapping
 	public Flux<String> check(){
+		log.info("Daniel log for testing on CGR");
 		return Flux.just("Emitted on : "+LocalDate.now().toString());
 	}
 }
